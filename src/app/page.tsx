b8a132b52b5f53ddc25b72a89ad763d0d8f5f272
Pusat Coding <pusatcoding1@gmail.com>
2026-05-14 23:12:50 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -133,7 +133,7 @@ export default function Portfolio() {
           <div className="social-media">
             <a href="https://github.com/pusatcoding1" target="_blank" rel="noopener noreferrer"><i className='bx bxl-github'></i></a>
             <a href="https://www.instagram.com/pusatcoding?igsh=MWwxb3F6MjVhOXNjMQ==" target="_blank" rel="noopener noreferrer"><i className='bx bxl-instagram-alt'></i></a>
-            <a href="tiktok.com/@pusat.coding" target="_blank" rel="noopener noreferrer"><i className='bx bxl-tiktok'></i></a>
+            <a href="https://www.tiktok.com/@pusat.coding" target="_blank" rel="noopener noreferrer"><i className='bx bxl-tiktok'></i></a>
           </div>
           <a href="#portfolio" className="btn">Proyek Kami</a>
         </div>
